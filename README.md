664d00b394e9e61e54fb8d15e5d2a296b9249e0e
mtdukes <tyler.dukes@gmail.com>
2015-05-16 20:04:25 -0400

    storing dr code

---
+++ README.md
@@ -0,0 +1,19 @@
+#Donor Reveal
+
+Donor-reveal is code that allows users to instantly reveal who donated to any current lawmakers mentioned in a story filed in @NCCapitol. It uses data from the National Institute on Money in State Politics.
+
+By hovering over a lawmaker's name, a pop-up will show basic information, as well as donation totals and the lawmaker's top five contributors.
+
+See an example here:
+
+http://wral.com/14509748
+
+## How it works
+
+The most basic building block of donor-reveal data is WRAL's dataset of current state lawmakers in the House and Senate.
+
+Donor-reveal relies heavily on campaign funding data from the [National Institute on Money in State Politics](http://followthemoney.org) and its open application programming interface, or API. Every night around 10 p.m., WRAL.com runs "data ingestors" that gather the most recently updated information available about each of the state's 170 state House and Senate members based on the names and IDs listed in WRAL's lawmakers dataset. The ingestors capture information on the each lawmaker's top-50 donors from latest election cycle, the date the data was last updated and, from a separate API endpoint, the total amount of donations the lawmaker received since the last election cycle.
+
+This data is stored automatically in a dataset asset in Diesel, with each lawmaker's information stored under their distinct "entity id."
+
+When a user loads a story filed in @NCCapitol, the donor-reveal script tries to find any full name listed in the lawmaker dataset (names are case-sensitive and must be spelled exactly the same as those in the database). If names are found, the script uses special highlighting on the text. It retrieves the bio information, contribution totals and top-five contributors from the data store and feeds them into a box that will appear when users hover over the highlighted text (or click/tap on mobile).
---
+++ donor_reveal.js
@@ -0,0 +1,152 @@
+/*donor-reveal_v0.8
+ Allows users to instantly reveal who donated to any current lawmakers
+ mentioned in a story filed in @NCCapitol.
+ It uses data from the National Institute on Money in State Politics
+ */
+require([
+    'jquery',
+    'lodash',
+    'jquery-ui'
+], function ($, _) {
+
+    //Global variables
+    var ncga_members = "not run";
+    var ftm_data;
+    var ftm_total;
+    var pol_data = {};
+    var opened_tooltip;
+
+    //Load members of the NCGA
+    var ncga_members = (function () {
+        $.ajax({
+            'async': true,
+            'global': false,
+            'url': '/news/state/nccapitol/data_set/14376504/?dsi_id=ncga-eid&version=jsonObj',
+            'dataType': "json",
+            'success': function (data) {
+                ncga_members = data;
+                highlightText();
+            }
+        });
+        return ncga_members;
+    })();
+
+    //Query the FollowTheMoney.com API
+    function getDonations(eid, wral_id) {
+        $.ajax({
+            'type': 'GET',
+            'async': true,
+            'url': '/news/state/nccapitol/data_set/14563916/?dsi_id=' + eid + '&version=jsonObj',
+            'dataType': "json",
+            'success': function (data) {
+                //clear old data
+                ftm_data = "";
+                //just load the first five items for display
+                $.each(data.slice(0, 5), function (index, value) {
+                    //If statement here checks for lawmakers with no records
+                    if (value["total"] == "No Records") {
+                        ftm_data = '<tr><td>No records</td><td>$0</td></tr>';
+                        ftm_total = '$0';
+                    }
+                    else {
+                        ftm_data += '<tr><td>' + (value["contributor"]).replace(/\w\S*/g, function (txt) {
+                            return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+                        }) +
+                        '</td><td>$' + (Math.round(value["total"]) + "").replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1,") + '</td></tr>';
+                        ftm_total = '$' + (Math.round(data[0]["total_hide"]) + "").replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1,");
+                    }
+                });
+                pol_data[wral_id][0] = ftm_total;
+                //add value to the pol_data object
+                pol_data[wral_id][1] = ftm_data;
+                //add value info was last updated
+                pol_data[wral_id][2] = data[0]["updated_hide"];
+                //when finished, close the tool tip and reopen by replacing the spinner with real data
+                $(".donor-reveal").tooltip('close');
+                $(".donor-reveal#" + opened_tooltip).tooltip('open');
+            }
+        });
+    }
+
+    //define tooltip structure and pass in variables
+    function ncgaBox(name, party, district, img, position, wral_id) {
+        return '<div class="donor-box">' +
+            '<img src="' + img + '">' +
+            '<p><span class="law-header">' + position + '</span><span class="breakpoint-phone-and-tablet-only" id="close-dr">&nbsp;X&nbsp;</span><br />' +
+            '<span class="law-name">' + name + '</span><br />' +
+            '<span class="party-dist">' + party + '-' + district + '</span>' +
+            '<p><span class="law-header">2013-14 donations:</span><br />' +
+            pol_data[wral_id][0] + '</p>' +
+            '<p style="clear:both;text-align:center;"><span class="law-header">Top donors since 2013</span></p>' +
+            '<table class="table">' +
+            '<thead><tr>' +
+            '<th>Contributor</th><th>Total</th>' +
+            '</thead></tr>' +
+            '<tbody>' +
+            pol_data[wral_id][1] +
+            '</tbody></table>' +
+            '<p class="source">Source: followthemoney.org<br />' +
+            'Data as of ' + pol_data[wral_id][2] + '</p>' +
+            '</div>';
+    }
+
+    //For clicking/tapping to close
+    $('.donor-box').click(function () {
+        $(".donor-reveal").tooltip('close');
+    });
+
+    //Script to find lawmakers and initialize distinct tooltips
+    function highlightText() {
+        //Make a list of lawmakers from the loaded JSON, then search for them
+        for (var i = 0; i < ncga_members.length; i++) {
+            $.each($(".story-text").children(), function() {
+                //skip embedded content
+                if ($(this).hasClass('embed-content')) {
+                    return;
+                }
+
+                $(this).html(
+                    $(this).html().replace(
+                        ncga_members[i]["member"],
+                        '<button id="' + i + '" class="donor-reveal">' + ncga_members[i]["member"] + '</button>'
+                    )
+                );
+
+                //and check for alternates, but only if the alt_spelling column isn't blank
+                if (ncga_members[i]["alt_spelling"] != "") {
+                    $(this).html(
+                        $(this).html().replace(
+                            ncga_members[i]["alt_spelling"],
+                            '<button id="' + i + '" class="donor-reveal">' + ncga_members[i]["alt_spelling"] + '</button>'
+                        )
+                    );
+                }
+            });
+        }
+
+        //On successful highlight, grab all IDs with class donor-reveal and get JSON data
+        $.each($('.donor-reveal'), function () {
+            //initialize with a loading spinner
+            pol_data[this.id] = ["", '<tr><td colspan="2" style="line-height:31px;"><img src="http://wwwcache.wral.com/presentation/v3/images/widgets/wait_popup/spinner.gif" style="width:31px;" />Loading...</td></tr>', ""];
+            getDonations(ncga_members[this.id]["eid"], this.id)
+        });
+
+        //add tooltip
+        $(".donor-reveal").tooltip({
+            items: "button",
+            open: function () {
+                opened_tooltip = $(this).attr('id')
+            },
+            content: function () {
+                var id = $(this).attr('id')
+                return ncgaBox(ncga_members[id]["member"],
+                    ncga_members[id]["party"],
+                    ncga_members[id]["county_short"],
+                    ncga_members[id]["headshot"],
+                    ncga_members[id]["title"],
+                    id
+                );
+            }
+        });
+    }
+});
